879f31d21f03e90d8d2e8481a27a604e8b34eef1
Marla <marla@brounie.com>
2019-10-23 18:48:03 -0500

    scanning

--- context-Free.py
+++ context-Free.py
@@ -6,9 +6,35 @@ rules = [[] for j in range(numberOfVariable)]
 
 variable = []
 terminals = []
+rules = []
 
-print("number of terminal", numberOfTerminals)
-for i in range(numberOfTerminals):
-    print("Set terminal "+str(i)+" :")
+#ESCANEO DE LAS VARIABLES(NON-TERMINAL SYMBOLS)
+for i in range(0, numberOfVariable):
+    print("Set variables "+str(i+1)+" :")
+    n=str(input())
+    variable.append(n)
+variable.append("empty")
+print(variable)
+
+
+#ESCANEO DE LAS TERMINALES
+for i in range(0, numberOfTerminals):
+    print("Set terminal "+str(i+1)+" :")
+    n=str(input())
+    terminals.append(n.upper())
 
 print(terminals)
+
+#ESCANEO DE LAS REGLAS  
+for i in range(0, numberOfRules):
+    aux = []
+    print("Which terminal use this rule: ")
+    print(terminals)
+    t=str(input())
+    aux.append(t)
+    print("Rule sring: ")
+    n=str(input())
+    aux.append(n)
+    rules.append(aux)
+
+print(rules)
